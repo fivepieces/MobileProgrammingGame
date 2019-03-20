9f3481d6dfd10310d71cb7bf81c4b24fec5f7e93
Keith <kwcotnoir@gmail.com>
2019-03-19 20:14:10 -0400

    Connection to FireBase
    Updated with connection to FireBase

---
+++ MobileGame/Pods/Target Support Files/Pods-MobileGame/Pods-MobileGame-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MobileGame : NSObject
+@end
+@implementation PodsDummy_Pods_MobileGame
+@end
---
+++ MobileGame/Pods/Target Support Files/Pods-MobileGameUITests/Pods-MobileGameUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MobileGameUITests : NSObject
+@end
+@implementation PodsDummy_Pods_MobileGameUITests
+@end
